906b345f298806fb320e3f983a04149b3b3d2a84
Ezeaccer <ezeacce@hotmail.com>
2014-09-30 10:48:30 -0300

    Modificaciones para que en sel.out indique cuando no se puede resolver el sistema.

--- TP2-Obligatorio/Sel/src/sel/Sel.java
+++ TP2-Obligatorio/Sel/src/sel/Sel.java
@@ -39,34 +39,43 @@ public class Sel {
 		}
 	}
 
-	public void resolverSistema() {
+	public boolean resolverSistema() {
 		vectorColumnaSolucion = new MatrizMath(vectorResultado.getDimension(),
 				1);
 		try {
-			vectorColumnaSolucion = matrizCoeficientes.inversa()
-					.multiplicar(vectorResultado.toMatrizMathColumna());
+			vectorColumnaSolucion = matrizCoeficientes.inversa().multiplicar(
+					vectorResultado.toMatrizMathColumna());
 		} catch (Exception e) {
 			e.printStackTrace();
+			return false;
 		}
+		return true;
 	}
 
 	public Double calcularError() {
-		MatrizMath resultadoMultiplicacion = matrizCoeficientes.multiplicar(vectorColumnaSolucion);
-		double error = vectorResultado.toMatrizMathColumna().restar(resultadoMultiplicacion).normaDos();
+		MatrizMath resultadoMultiplicacion = matrizCoeficientes
+				.multiplicar(vectorColumnaSolucion);
+		double error = vectorResultado.toMatrizMathColumna()
+				.restar(resultadoMultiplicacion).normaDos();
 		return error;
 	}
-	
-	public void guardarSolucion(String nombre) {
+
+	public void guardarSolucion(String nombre, boolean marca) {
 		CreateFile archivo = null;
 
 		try {
 			archivo = new CreateFile(nombre);
-			archivo.writeLine(vectorColumnaSolucion.getFilas().toString());
-			for (int i = 0; i < vectorColumnaSolucion.getFilas(); i++) {
-				archivo.writeLine(vectorColumnaSolucion.getMatriz()[i][0].toString());
+			if (marca == false)
+				archivo.writeLine("El sistema no tiene solución.");
+			else {
+				archivo.writeLine(vectorColumnaSolucion.getFilas().toString());
+				for (int i = 0; i < vectorColumnaSolucion.getFilas(); i++) {
+					archivo.writeLine(vectorColumnaSolucion.getMatriz()[i][0]
+							.toString());
+				}
+				archivo.writeLine("");
+				archivo.writeLine(calcularError().toString());
 			}
-			archivo.writeLine("");
-			archivo.writeLine(calcularError().toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -82,17 +91,19 @@ public class Sel {
 
 	public static void main(String[] args) {
 		Sel sel = new Sel("sel.in");
-
+		
+		boolean marca;
+		
 		Calendar tIni = new GregorianCalendar();
 		
-		sel.resolverSistema();
+		marca = sel.resolverSistema();
 		
 		Calendar tFin = new GregorianCalendar();
 		
 		long diff = tFin.getTimeInMillis() - tIni.getTimeInMillis();
 
 		System.out.println(Math.round(diff / 1000));
-		
-		sel.guardarSolucion("sel.out");
+
+		sel.guardarSolucion("sel.out", marca);
 	}
 }
