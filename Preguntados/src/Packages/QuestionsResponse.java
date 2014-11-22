3a35878314d145c2060611b52076aa5671945b32
Ezeaccer <ezeacce@hotmail.com>
2014-11-22 10:47:50 -0300

    Fix preguntas hardcodeadas de prueba

--- Preguntados/src/Packages/QuestionsResponse.java
+++ Preguntados/src/Packages/QuestionsResponse.java
@@ -14,17 +14,13 @@ public class QuestionsResponse implements Serializable, Package {
 	public QuestionsResponse (){
 		this.questions = new ArrayList<Question> ();
 		
-<<<<<<< HEAD
-		questions.add(new Question("Qué equipo pechea siempre al final del torneo?"));
-		questions.add(new Question("Quién descubrió el continente de zona sur?"));
-		questions.add(new Question("Quien no va a dejar nunca la falopa"));
-		questions.add(new Question("La anterior pregunta tiene como respuesta al diego?"));
-=======
-		questions.add(new StringQuestion("Quï¿½ equipo pechea siempre al final del torneo?", 1));
-		questions.add(new StringQuestion("Quiï¿½n descubriï¿½ el continente de zona sur?", 2));
-		questions.add(new StringQuestion("Quien no va a dejar nunca la falopa", 3));
-		questions.add(new StringQuestion("La anterior pregunta tiene como respuesta al diego?", 4));
->>>>>>> origin/master
+
+		questions.add(new Question(1,"Qué equipo pechea siempre al final del torneo?"));
+		questions.add(new Question(2,"Quién descubrió el continente de zona sur?"));
+		questions.add(new Question(3,"Quien no va a dejar nunca la falopa"));
+		questions.add(new Question(4,"La anterior pregunta tiene como respuesta al diego?"));
+
+
 		
 	}
 	
