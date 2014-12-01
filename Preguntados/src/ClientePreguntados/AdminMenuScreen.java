ca4304a0198d277a9cd67cd13a574bda48460ddc
Ezeaccer <ezeacce@hotmail.com>
2014-12-01 11:10:45 -0300

    Se limpia codigo

--- Preguntados/src/ClientePreguntados/AdminMenuScreen.java
+++ Preguntados/src/ClientePreguntados/AdminMenuScreen.java
@@ -25,18 +25,6 @@ public class AdminMenuScreen extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	// public static void main(String[] args) {
-	// EventQueue.invokeLater(new Runnable() {
-	// public void run() {
-	// try {
-	// AdminMenuScreen frame = new AdminMenuScreen();
-	// frame.setVisible(true);
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// }
-	// });
-	// }
 
 	/**
 	 * Create the frame.
