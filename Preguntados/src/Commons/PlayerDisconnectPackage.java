11823e616b63497f0e022cc819a4b949d02fa163
felizagoyen <felizagoyen@despegar.com>
2014-12-06 00:44:16 -0300

    Se agrega boolean que indica si la partida fue cancelada

--- Preguntados/src/Commons/PlayerDisconnectPackage.java
+++ Preguntados/src/Commons/PlayerDisconnectPackage.java
@@ -7,9 +7,14 @@ public class PlayerDisconnectPackage implements Serializable, Package {
 	private static final long serialVersionUID = -3719603863382808234L;
 	private static final Integer PACKAGEID = 16;
 	private String userName;
+	private Boolean gameCanceled;
 	
-	public PlayerDisconnectPackage(String userName) {
+	public PlayerDisconnectPackage() {
+	}
+	
+	public PlayerDisconnectPackage(String userName, Boolean gameCanceled) {
 		this.userName = userName;
+		this.gameCanceled = gameCanceled;
 	}
 	
 	public Integer getPackageID() {
@@ -20,4 +25,8 @@ public class PlayerDisconnectPackage implements Serializable, Package {
 		return userName;
 	}
 
+	public Boolean gameCanceled() {
+		return gameCanceled;
+	}
+	
 }
